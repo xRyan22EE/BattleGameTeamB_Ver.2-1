5b5555a30b222e856febdb60a225c07f0e809e99
Ryan <152604843+xRyan22EE@users.noreply.github.com>
2024-11-22 16:28:58 +0300

    new v

---
+++ Data/Ai.py
@@ -0,0 +1,169 @@
+import random
+from time import sleep
+
+import pygame
+
+
+class ai_thinking():
+    def __init__(self,grid_size, ai_values, player1_values):
+        self.last = pygame.time.get_ticks()
+        self.cooldown = 5
+        self.grid_size = grid_size
+        self.ai_values = ai_values
+        self.player1_values = player1_values
+
+    def fire(self):
+        # fire gun, only if cooldown has been 2 seconds since last
+        now = pygame.time.get_ticks()
+        if now - self.last >= self.cooldown:
+            self.last = now
+            return ai_level1(self.grid_size, self.ai_values, self.player1_values)
+
+def ai_level1(grid_size, ai_values, player1_values):
+    xCooForShots = random.randrange(grid_size)
+    yCooForShots = random.randrange(grid_size)
+    while (xCooForShots, yCooForShots) in ai_values['shots_taken']:
+        xCooForShots = random.randrange(grid_size)
+        yCooForShots = random.randrange(grid_size)
+    else:
+        ai_values['shots_taken'].append((xCooForShots, yCooForShots))
+    
+    return xCooForShots, yCooForShots , ai_values
+
+def ai_level2(grid_size, ai_values, player1_values):
+
+    # ai_values['unknown_cells'] = []
+    # for i in range(grid_size):
+    #     ai_values['unknown_cells'].append(['..' for j in range(grid_size)])
+
+    # Set opponent values
+    player_grid = player1_values['player1Grid']
+    xCooForShots, yCooForShots = player1_values['last_hit']
+    first_hit = ai_values['first_hit']
+
+    print('------------------------------------------')
+    print(xCooForShots, yCooForShots)
+    print(ai_values['shots_taken'])
+
+    print(player_grid[xCooForShots][yCooForShots])
+
+    print(player1_values['last_hit'])
+    print(ai_values['last_hit'])
+
+
+    last_hit = ''
+
+    try:
+        # Search if last try is a hit
+        if player_grid[xCooForShots][yCooForShots] == 'H':
+            print("enter")
+            # make the next shot adjacent to the last shot
+            # if last hit is right, continue to hit the right cell
+            if ai_values['last_hit'] == 'right':
+                # Reset possibilities
+                possibly_hits = []
+                possibly_hits.append((xCooForShots + 1, yCooForShots))
+                last_hit = 'right'
+                ai_values['last_hit'] = last_hit
+                first_hit = False
+
+            # if last hit is right, continue to hit the right cell
+            elif ai_values['last_hit'] == 'left':
+                # Reset possibilities
+                possibly_hits = []
+                possibly_hits.append((xCooForShots - 1, yCooForShots))
+                last_hit = 'left'
+                ai_values['last_hit'] = last_hit
+                first_hit = False
+
+            # if last hit is right, continue to hit the right cell
+            elif ai_values['last_hit'] == 'down':
+                # Reset possibilities
+                possibly_hits = []
+                possibly_hits.append((xCooForShots, yCooForShots - 1))
+                last_hit = 'down'
+                ai_values['last_hit'] = last_hit
+                first_hit = False
+
+            # if last hit is right, continue to hit the right cell
+            elif ai_values['last_hit'] == 'up':
+                # Reset possibilities
+                possibly_hits = []
+                possibly_hits.append((xCooForShots, yCooForShots + 1))
+                last_hit = 'up'
+                ai_values['last_hit'] = last_hit
+                first_hit = False
+
+            # if it's a new hit, try all sides
+            else:
+                # Reset possibilities
+                possibly_hits = []
+                print("don't know the previous hit")
+                possibly_hits.append((xCooForShots + 1, yCooForShots))
+                possibly_hits.append((xCooForShots - 1, yCooForShots))
+                possibly_hits.append((xCooForShots, yCooForShots - 1))
+                possibly_hits.append((xCooForShots, yCooForShots + 1))
+
+                ai_values['last_hit'] = last_hit
+
+                first_hit = True
+
+            # search if adjacent is not taken
+            if not first_hit:
+                for hit in possibly_hits:
+                    print(hit)
+                    if hit not in ai_values['shots_taken']:
+                        if (hit[0], hit[1]) == (xCooForShots + 1, yCooForShots):
+                            last_hit = 'right'
+                            ai_values['last_hit'] = last_hit
+                            if 0 <= hit[0] < 10 and 0 <= hit[1] < 10:
+                                xCooForShots, yCooForShots = hit[0], hit[1]
+
+                        elif (hit[0], hit[1]) == (xCooForShots - 1, yCooForShots):
+                            last_hit = 'left'
+                            ai_values['last_hit'] = last_hit
+                            if 0 <= hit[0] < 10 and 0 <= hit[1] < 10:
+                                xCooForShots, yCooForShots = hit[0], hit[1]
+
+                        elif (hit[0], hit[1]) == (xCooForShots, yCooForShots - 1):
+                            last_hit = 'down'
+                            ai_values['last_hit'] = last_hit
+                            if 0 <= hit[0] < 10 and 0 <= hit[1] < 10:
+                                xCooForShots, yCooForShots = hit[0], hit[1]
+
+                        elif (hit[0], hit[1]) == (xCooForShots, yCooForShots + 1):
+                            last_hit = 'up'
+                            ai_values['last_hit'] = last_hit
+                            if 0 <= hit[0] < 10 and 0 <= hit[1] < 10:
+                                xCooForShots, yCooForShots = hit[0], hit[1]
+
+            else:
+                if ai_values['tries'] <= 4 :
+                    hit = (possibly_hits[ai_values['tries']][0], possibly_hits[ai_values['tries']][1])
+                    if hit not in ai_values['shots_taken']:
+                        if 0 <= hit[0] < 10 and 0 <= hit[1] < 10:
+                            yCooForShots, xCooForShots = hit[0], hit[1]
+                        print(xCooForShots, yCooForShots)
+                    ai_values['tries'] += 1
+                else:
+                    ai_values['tries'] = 0
+
+
+
+        else:  # if not hit, continue searching
+            print("in")
+            print(xCooForShots, yCooForShots, 'before')
+            xCooForShots = random.randrange(grid_size)
+            yCooForShots = random.randrange(grid_size)
+            while (xCooForShots, yCooForShots) in ai_values['shots_taken']:
+                xCooForShots = random.randrange(grid_size)
+                yCooForShots = random.randrange(grid_size)
+            else:
+                ai_values['shots_taken'].append((xCooForShots, yCooForShots))
+            print(xCooForShots, yCooForShots, 'after')
+    except:
+        pass
+    print('------------------------------------------')
+    print(ai_values)
+    print('------------------------------------------')
+    return xCooForShots, yCooForShots, ai_values
---
+++ Data/button.py
@@ -0,0 +1,81 @@
+import pygame
+
+class Button():
+	def __init__(self, image, pos, text_input, font, base_color, hovering_color):
+		self.image = image
+		self.x_pos = pos[0]
+		self.y_pos = pos[1]
+		self.font = font
+		self.base_color, self.hovering_color = base_color, hovering_color
+		self.text_input = text_input
+		self.text = self.font.render(self.text_input, True, self.base_color)
+		if self.image is None:
+			self.image = self.text
+		self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
+		self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
+
+	def update(self, screen):
+		if self.image is not None:
+			screen.blit(self.image, self.rect)
+		screen.blit(self.text, self.text_rect)
+
+	def checkForInput(self, position):
+		if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
+			return True
+		return False
+
+	def changeColor(self, position):
+		if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
+			self.text = self.font.render(self.text_input, True, self.hovering_color)
+		else:
+			self.text = self.font.render(self.text_input, True, self.base_color)
+
+BUTTONSTATES = {True: "black", False: "White"}
+
+class Slider():
+    def __init__(self, pos: tuple, size: tuple, initial_val: float, min: int, max: int) -> None:
+        self.pos = pos
+        self.size = size
+        self.hovered = False
+        self.grabbed = False
+
+        self.slider_left_pos = self.pos[0] - (size[0] // 2)
+        self.slider_right_pos = self.pos[0] + (size[0] // 2)
+        self.slider_top_pos = self.pos[1] - (size[1] // 2)
+
+        self.min = min
+        self.max = max
+        self.button_width = 20  # Set the button width here
+        self.initial_val = (self.slider_right_pos - self.slider_left_pos - self.button_width) * initial_val  # Adjust for button width
+
+        self.container_rect = pygame.Rect(self.slider_left_pos, self.slider_top_pos, self.size[0], self.size[1])
+        self.button_rect = pygame.Rect(self.slider_left_pos + self.initial_val, self.slider_top_pos, self.button_width, self.size[1])
+
+        # label
+        self.text = pygame.font.Font(r"images\assets\font.ttf", 25).render(str(int(self.get_value())), True, "white", None)
+        self.label_rect = self.text.get_rect(center=(self.pos[0], self.slider_top_pos - 15))
+
+    def move_slider(self, mouse_pos):
+        pos = mouse_pos[0]
+        if pos < self.slider_left_pos + self.button_width // 2:
+            pos = self.slider_left_pos + self.button_width // 2
+        if pos > self.slider_right_pos - self.button_width // 2:
+            pos = self.slider_right_pos - self.button_width // 2
+        self.button_rect.centerx = pos  # Adjust the centerx position directly
+
+    def hover(self):
+        self.hovered = True
+
+    def render(self, app):
+        pygame.draw.rect(app, "Green", self.container_rect)
+        pygame.draw.rect(app, BUTTONSTATES[self.hovered], self.button_rect)
+
+    def get_value(self):
+        val_range = self.slider_right_pos - self.slider_left_pos - self.button_width  # Adjust for button width
+        button_val = self.button_rect.centerx - self.slider_left_pos - self.button_width // 2
+
+        return (button_val / val_range) * (self.max - self.min) + self.min
+
+    def display_value(self, app):
+        self.text = pygame.font.Font(r"images\assets\font.ttf", 25).render(str(int(self.get_value())), True, "white", None)
+        app.blit(self.text, self.label_rect)
